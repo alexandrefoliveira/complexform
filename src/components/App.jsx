35a78f200dac2008e5700096ec6ac11e4a9ba8a7
Alexandre Oliveira <alexandreoliveira@Alexandres-MacBook-Pro.local>
2020-05-30 20:30:39 -0400

    Complex Form, using useState() - Option2 - Added a new email field.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,8 @@ import React, { useState } from "react";
 function App() {
     const [fullName , setFullName] = useState({
         fName: "",
-        lName: ""
+        lName: "",
+        eMail: ""
     });
 
     function handleChange(event){
@@ -13,12 +14,20 @@ function App() {
             if(name === "fName"){
                 return{
                     fName : value,
-                    lName : prevValue.lName
+                    lName : prevValue.lName,
+                    eMail : prevValue.eMail
                 }
             }else if (name === "lName"){
                 return{
                     fName : prevValue.fName,
-                    lName : value
+                    lName : value,
+                    eMail : prevValue.eMail
+                }
+            }else if (name === "eMail"){
+                return{
+                    fName : prevValue.fName,
+                    lName : prevValue.lName,
+                    eMail : value
                 }
             }
         })
@@ -26,10 +35,12 @@ function App() {
 
   return (
     <div className="container">
-      <h1>Hello {fullName.fName} {fullName.lName} </h1>
+      <h1>Hello {fullName.fName} {fullName.lName}</h1>
+      <p> {fullName.eMail}</p>
       <form>
         <input name="fName" onChange={handleChange} placeholder="First Name" value={fullName.fName}/>
         <input name="lName" onChange={handleChange} placeholder="Last Name" value={fullName.lName}/>
+        <input name="eMail" onChange={handleChange} placeholder="Email" value={fullName.email}/>
         <button>Submit</button>
       </form>
     </div>
